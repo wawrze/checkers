a0c11d2bf9467b245242e309f4f2cae5fc948d66
Mateusz Wawreszuk <mateusz.wawreszuk@gmail.com>
2019-10-26 15:04:08 +0200

    windows interface init

--- src/main/java/checkers/Checkers.java
+++ src/main/java/checkers/Checkers.java
@@ -1,18 +1,47 @@
 package checkers;
 
-import checkers.ui.Menu;
-import exceptions.IncorrectMoveException;
-import exceptions.IncorrectMoveFormat;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-import java.io.IOException;
+public class Checkers extends Application {
 
-class Checkers {
+    private Stage window;
+    private Button button;
 
-    public static void main(String[] args)
-            throws IncorrectMoveFormat, IncorrectMoveException, IOException, ClassNotFoundException {
-        (new Menu()).start();
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("Checkers");
+        button = new Button("Button");
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 1200, 800);
+        window.setScene(scene);
+        window.show();
     }
 
 }
+
+//import checkers.ui.Menu;
+//import exceptions.IncorrectMoveException;
+//import exceptions.IncorrectMoveFormat;
+//
+//import java.io.IOException;
+//
+//class Checkers {
+//
+//    public static void main(String[] args)
+//            throws IncorrectMoveFormat, IncorrectMoveException, IOException, ClassNotFoundException {
+//        (new Menu()).start();
+//    }
+//
+//}
 
 //♙♘♗♖♕♔♟♞♝♜♛♚
